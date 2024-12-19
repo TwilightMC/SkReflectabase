bb04ec8f4a150fbfe9335ab89fc9029c96c75740
Pixity <81818297+PixityFlixity@users.noreply.github.com>
2024-12-18 19:48:39 -0500

    Javadocs

---
+++ Javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"net.twilightmc.skReflectabase","l":"DatabaseManager"},{"p":"net.twilightmc.skReflectabase","l":"DatabaseManager.QueryResult"},{"p":"net.twilightmc.skReflectabase","l":"SkReflectabase"}];updateSearchResults();
